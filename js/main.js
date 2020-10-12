19ddb6c569b455fdada3929d73f1d06ac7cf856c
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-10-12 21:16:37 +0500

    WIP maketda berilgan breakpointlarga moslandi, nojs holatiga moslandi

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+var elSiteBody = document.querySelector('.page__body');
 var elSiteNav = document.querySelector('.site-nav');
-
 var elSiteNavToggler = document.querySelector('.site-nav__toggler-button');
+
+elSiteBody.classList.remove('page__body--nojs');
 
 elSiteNavToggler.addEventListener('click', function () {
 
